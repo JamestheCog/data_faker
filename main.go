bd44a9fc7b4b97ee82de305155b03679a3b82a04
JamestheCog <jcog196013@gmail.com>
2026-05-09 03:18:03 +0800

    feat: changed CLI app's arguments so that they're easier to work with

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"runtime"
@@ -16,20 +17,41 @@ import (
 // helper function elsewhere) when the utilities needed to do so are already present PLUS
 // it's not meaningfully different like the other modes.
 func main() {
-	dest := flag.String("dest", "", "The endpoint or URL to send fake data to.")
-	sendMode := flag.String("mode", "single", "The kind of request to simulate ('single', 'batch', 'high_vol', and 'concurrent').")
-	numRequests := flag.Int("num_requests", 5, "How many requests should be sent (only applicable for batch requests)?")
-	requestDuration := flag.Int("duration", 10, "How long should requests be sent for (only applicable for streaming requests)?")
-	numWorkers := flag.Int("num_workers", max(1, runtime.NumCPU()-3), "The amount of concurrent processes to spawn for concurrent sending")
+	var (
+		dest, mode                                           string
+		numRequests, requestDuration, numWorkers, defWorkers int
+	)
+	defWorkers = max(1, runtime.NumCPU()-2)
+	flag.StringVar(&dest, "l", "", "")
+	flag.StringVar(&dest, "location", "", "")
+	flag.StringVar(&mode, "m", "single", "")
+	flag.StringVar(&mode, "mode", "single", "")
+	flag.IntVar(&numRequests, "nr", 5, "")
+	flag.IntVar(&numRequests, "num_reqs", 5, "")
+	flag.IntVar(&requestDuration, "rd", 10, "")
+	flag.IntVar(&requestDuration, "duration", 10, "")
+	flag.IntVar(&numWorkers, "nw", defWorkers, "")
+	flag.IntVar(&numWorkers, "num_workers", defWorkers, "")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage instructions for %s:\n", os.Args[0])
+		fmt.Fprint(os.Stderr, "  -l, -location string\n    \tThe destination address you want the payload sent to.\n")
+		fmt.Fprint(os.Stderr, "  -m, -mode string\n    \tThe sending mode you want the CLI app to simulate: `single`, `batch`, `high_vol`, or "+
+			"`concurrent` (default: single).\n")
+		fmt.Fprint(os.Stderr, "  -nr, -num_reqs integer\n    \tThe amount of requests to batch-send during `batch` sending mode (default: 5).\n")
+		fmt.Fprint(os.Stderr, "  -d, -duration string\n    \tHow many seconds should the `high_vol` or `concurrent` mode be sending for (default: 10 seconds)?\n")
+		fmt.Fprintf(os.Stderr, "  -nw, -num_workers integer\n    \tThe amount of workers to use during `concurrent` sending mode (default: %d workers).\n",
+			defWorkers)
+	}
 	flag.Parse()
 
-	sendChoice := strings.TrimSpace(strings.ToLower(*sendMode))
+	sendChoice := strings.TrimSpace(strings.ToLower(mode))
 
-	if *dest == "" || *sendMode == "" || *numRequests == 0 || *requestDuration == 0 || *numWorkers == 0 {
+	if dest == "" || mode == "" || numRequests == 0 || requestDuration == 0 || numWorkers == 0 {
 		flag.Usage()
 		os.Exit(0)
 	}
-	if *numWorkers == 1 && sendChoice == "concurrent" {
+	if numWorkers == 1 && sendChoice == "concurrent" {
 		log.Fatalln("Your machine's too weak to be doing things concurrently - try one of the other modes instead!")
 	}
 
@@ -39,14 +61,14 @@ func main() {
 		if err != nil {
 			log.Fatalf("Could not send the payload for the following reason: %v", err)
 		}
-		senders.SendPayload(*dest, payload)
+		senders.SendPayload(dest, payload)
 	case "batch":
-		senders.SendBatch(*dest, *numRequests)
+		senders.SendBatch(dest, numRequests)
 	case "high_vol":
-		senders.HighVolume(*dest, *requestDuration)
+		senders.HighVolume(dest, requestDuration)
 	case "concurrent":
-		senders.ConcurrentSending(*dest, *numWorkers, *requestDuration)
+		senders.ConcurrentSending(dest, numWorkers, requestDuration)
 	default:
-		log.Fatalln("'mode' needs to be one of the following: 'single', 'batch', 'high_volume', or 'concurrent'!")
+		log.Fatalln("`mode` or `m` needs to be one of the following: `single`, `batch`, `high_vol`, or `concurrent`!")
 	}
 }
